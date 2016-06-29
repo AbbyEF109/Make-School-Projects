2b316723844af6c0a69d5d5144e17e84576ff8f1
AbbyEF109 <abbyef109@gmail.com>
2016-06-29 12:03:12 -0400

    Added two new functions from the tutorial

--- MakeSchoolNotes-Swift-V2-Starter-master 2/MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes-Swift-V2-Starter-master 2/MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -10,8 +10,21 @@ import UIKit
 
 class ListNotesTableViewController: UITableViewController {
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
   }
-  
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath)
+        
+        
+        cell.textLabel?.text = "Yay - it's working!"
+        
+        return cell
+    }
 }
