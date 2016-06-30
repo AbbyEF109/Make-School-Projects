77dceff0527677d4b1baf2cbfc2959486a86d51c
AbbyEF109 <abbyef109@gmail.com>
2016-06-30 10:06:13 -0400

    Defined the Notes class in the new Note model

---
+++ MakeSchoolNotes-Swift-V2-Starter-master 2/MakeSchoolNotes/Models/Notes.swift
@@ -0,0 +1,14 @@
+//
+//  Notes.swift
+//  MakeSchoolNotes
+//
+//  Created by Abby on 6/30/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import Foundation
+class Note {
+    var title = ""
+    var content = ""
+    var modificationTime = NSDate()
+}
