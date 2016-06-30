fba4feb04882403389e48d0ff743afd74fde103a
AbbyEF109 <abbyef109@gmail.com>
2016-06-30 11:42:21 -0400

    Finished the functional Make Notes App. Next step is the challenges.

--- MakeSchoolNotes-Swift-V2-Starter-master 2/MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes-Swift-V2-Starter-master 2/MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -7,31 +7,57 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ListNotesTableViewController: UITableViewController {
+    
+    var notes: Results<Note>! {
+        didSet {
+            tableView.reloadData()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-  }
+        notes = RealmHelper.retrieveNotes()
+    }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return notes.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        // 1
         let cell = tableView.dequeueReusableCellWithIdentifier("listNotesTableViewCell", forIndexPath: indexPath) as! ListNotesTableViewCell
         
+        // 1
+        let row = indexPath.row
+        
         // 2
-        cell.noteTitleLabel.text = "note's title"
-        cell.noteModificationTimeLabel.text = "note's modification time"
+        let note = notes[row]
+        
+        // 3
+        cell.noteTitleLabel.text = note.title
+        
+        // 4
+        cell.noteModificationTimeLabel.text = note.modificationTime.convertToString()
         
         return cell
     }
+    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if let identifier = segue.identifier {
             if identifier == "displayNote" {
                 print("Table view cell tapped")
+                
+                // 1
+                let indexPath = tableView.indexPathForSelectedRow!
+                // 2
+                let note = notes[indexPath.row]
+                // 3
+                let displayNoteViewController = segue.destinationViewController as! DisplayNoteViewController
+                // 4
+                displayNoteViewController.note = note
+                
             } else if identifier == "addNote" {
                 print("+ button tapped")
             }
@@ -43,5 +69,15 @@ class ListNotesTableViewController: UITableViewController {
         // for now, simply defining the method is sufficient.
         // we'll add code later
         
+    }
+    // 1
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == .Delete {
+            //1
+            RealmHelper.deleteNote(notes[indexPath.row])
+            //2
+            notes = RealmHelper.retrieveNotes()
+        }
     }
 }
